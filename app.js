2ab5962b559412f36be3caab31d529e74602bf39
Himanshu Kesarwani <himanshukesarwani800@gmail.com>
2021-12-05 14:40:09 +0530

    taking input and printing output on console.

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("Hello World!");
+var btnTranslate = document.querySelector("#btn-translate");
+var textInput = document.querySelector("#text-input");
+
+
+function clickEventHandler() {
+        console.log("clicked");
+        console.log(textInput.value);
+    }
+
+btnTranslate.addEventListener('click', clickEventHandler)
+
+
+
