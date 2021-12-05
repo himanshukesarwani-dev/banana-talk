2faaeb7e29263d59bd56c24f50fbe24a34955693
Himanshu Kesarwani <himanshukesarwani800@gmail.com>
2021-12-05 15:53:40 +0530

    basic input output process done

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var textInput = document.querySelector("#text-input");
-
+var textOutput = document.querySelector("#text-output");
 
 function clickEventHandler() {
-        console.log("clicked");
-        console.log(textInput.value);
+       textOutput.innerText = textInput.value + "Translated Text will come here!";
     }
 
 btnTranslate.addEventListener('click', clickEventHandler)
